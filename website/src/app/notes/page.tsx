e33ebb843b1770adb604f0b492459d5cde5bdc00
JohnDonavon <john.stephens95@gmail.com>
2024-03-21 16:31:55 -0700

    Add testing website for miden wasm

---
+++ website/src/app/notes/page.tsx
@@ -0,0 +1,3 @@
+export default function Notes() {
+  return <div className="flex min-h-screen flex-col items-center justify-between p-24">This is the notes page</div>
+}
---
+++ website/src/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import DefaultLayout from "@/components/defaultLayout";
+
+export default function Home() {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-between p-24">
+      <p>This website will serve as a testing site and demo of the Miden WASM</p>
+    </div>
+  );
+}
