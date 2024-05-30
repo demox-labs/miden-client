0d8fc23099e345311dcf8ab1ac0dfcbed8a67f14
Evan <evangeorgemarshall@gmail.com>
2024-05-30 15:17:52 -0700

    update

--- client/src/lib.rs
+++ client/src/lib.rs
@@ -1,9 +1,12 @@
 extern crate alloc;
 
 pub mod client;
+pub use client::*;
 pub mod config;
 pub mod errors;
+pub use errors::*;
 pub mod store;
+pub use store::*;
 
 #[cfg(any(test, feature = "test_utils"))]
 pub mod mock;
--- wasm/src/lib.rs
+++ wasm/src/lib.rs
@@ -1,6 +1,7 @@
 extern crate alloc;
 
 pub mod web_client;
+pub use web_client::*;
 
 use wasm_bindgen::prelude::*;
 use wasm_bindgen_futures::*;
