c8f46901aa2757caafb63554a38c17ce48c9403b
Julian <julian@demoxlabs.xyz>
2024-08-21 11:09:20 -0500

    Adding a pattern to make tests more reusable

--- crates/web-client/test/poc.test.ts
+++ crates/web-client/test/poc.test.ts
@@ -1,3 +1,7 @@
+// @ts-nocheck
+// Due to the amount of context swapping we do in these tests, we need to disable typescript checking.
+// Typescript is being kept to improve development experience especially with the web client type.
+
 import { ChildProcess, exec } from "child_process";
 import puppeteer, { Browser, Page } from "puppeteer";
 import { expect } from "chai";
@@ -8,11 +12,28 @@ describe("WASM Integration Test", function () {
   let page: Page;
   let serverProcess: ChildProcess;
 
+  let client: wasmModuleType.WebClient;
+
   before(async function () {
     serverProcess = exec("http-server ./dist -p 8080");
-
     browser = await puppeteer.launch({ headless: true });
     page = await browser.newPage();
+    await page.goto("http://localhost:8080");
+
+    // Uncomment below to enable console logging
+    // page.on("console", (msg) => console.dir(msg.text(), { depth: null }));
+
+    await page.exposeFunction("create_client", async () => {
+      await page.evaluate(async () => {
+        const { WebClient } = await import("./index.js");
+        // let rpc_url = "http://18.203.155.106:57291";
+        let rpc_url = "http://localhost:57291";
+        const client: wasmModuleType.WebClient = new WebClient();
+        await client.create_client(rpc_url);
+
+        window.client = client;
+      });
+    });
   });
 
   after(async function () {
@@ -24,23 +45,12 @@ describe("WASM Integration Test", function () {
     this.timeout(10000);
     await page.goto("http://localhost:8080");
     const result = await page.evaluate(async () => {
-      // @ts-ignore: This is within the context of the web page not the editor
-      const wasmModule = await import("./index.js");
-
-      // let rpc_url = "http://18.203.155.106:57291";
-      let rpc_url = "http://localhost:57291";
-      const client: wasmModuleType.WebClient = new wasmModule.WebClient();
-      await client.create_client(rpc_url);
-
-      // Call a function from your WASM module
-
+      await window.create_client();
+      const client = window.client;
       const newWallet = client.new_wallet("OffChain", true);
 
       return newWallet;
     });
-    console.log({ result });
     expect(result).to.be.not.null;
   });
 });
-
-const createClient = async () => {};
