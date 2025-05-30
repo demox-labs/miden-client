21a4033f3cfeb7a1d84de0c893632462c126944d
Evan <evangeorgemarshall@gmail.com>
2025-05-29 22:11:16 -0700

    Fix wasm-opt

--- crates/web-client/rollup.config.js
+++ crates/web-client/rollup.config.js
@@ -42,15 +42,13 @@ export default [
       rust({
         extraArgs: {
           cargo: [
+            "--features",
+            "testing",
             "--config",
-            // `build.rustflags=["-C", "target-feature=+atomics,+bulk-memory,+mutable-globals", "-C", "link-arg=--max-memory=4294967296", "-C", "opt-level=3"]`,
             `build.rustflags=["-C", "target-feature=+atomics,+bulk-memory,+mutable-globals", "-C", "link-arg=--max-memory=4294967296"]`,
             "--no-default-features",
           ],
-          wasmOpt: testing
-            ? ["-O0", "--enable-threads", "--enable-bulk-memory-opt"]
-            // : ["-O4", "--enable-threads", "--enable-bulk-memory-opt", "--enable-reference-types"],
-            : ["--enable-threads", "--enable-bulk-memory-opt"],
+          wasmOpt: ["-O4", "--enable-bulk-memory", "--enable-nontrapping-float-to-int"],
         },
         experimental: {
           typescriptDeclarationDir: "dist/crates",
